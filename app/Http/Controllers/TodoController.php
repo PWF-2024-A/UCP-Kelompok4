b65545604370610ede1f8fa52f3810f9ca71e9f6
Amaad11 <muhammad.syafei.ft21@mail.umy.ac.id>
2024-03-23 16:14:08 +0700

    migration, database seeder, factory

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
     public function index()
     {
+        $todos = Todo::where('user_id', auth()->user()->id)->get();
+        dd($todos);
         return view('todo.index');
     }
     public function create()
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Todo;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +17,17 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'is_admin' => true,
+        ]);
+        User::factory()->create([
+            'name' => 'Muhammad Syafei',
+            'email' => 'muhammadsyafei@gmail.com',
+        ]);
+
+        User::factory(100)->create();
+        Todo::factory(500)->create();
     }
 }
